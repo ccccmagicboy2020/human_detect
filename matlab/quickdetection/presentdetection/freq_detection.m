477a12db449733a587c28eaa0584da724b2f4f8d
cccc <cuiwei_cv@163.com>
2020-11-18 11:14:47 +0800

    update to newest matlab files

--- matlab/quickdetection/presentdetection/freq_detection.m
+++ matlab/quickdetection/presentdetection/freq_detection.m
@@ -1,15 +1,18 @@
-function [freq_vote] = quick_detection_freq(data, win_size, stride, time_accum, xhz)
+function [freq_vote] = freq_detection(data, win_size_freq, stride_freq, time_accum, xhz, freq_times)
 
 %{
-Function Name: quick_detection_freq
+Function Name: freq_detection
 Description: 根据窗内均值返回频域判定结果
 Input: 
 	data: 一维数组
-	win_size: 窗长
-	stride: 步长
+	win_size_freq: 频域窗长
+	stride_freq: 频域步长
+	time_accum: 累积时间
+	xhz: 去除频点数量
+	freq_times: 频域乘法门限
 Output: None
 Return: 
-	time_vote: 频域判定结果（布尔值）
+	freq_vote: 频域判定结果（布尔值）
 %}
 
 w = window(@hamming, length(data)); % 窗函数
@@ -19,10 +22,10 @@ AP_double = abs(Y)/ length(data); % 双边谱
 AP_single = AP_double(1: length(data)/ 2); % 单边谱
 AP_single(2: end) = 2* AP_single(2: end); % 计算单边谱幅度并去除零频放大效应
 AP_single_ANF = remove_pf(AP_single, time_accum, xhz); % 去除工频及其谐波周围2Hz频点
-quick_freq = zeros((length(AP_single_ANF)- win_size)/ stride+ 1, 1); % 1秒滑窗 数据量等于sample_rate/2 步长为0.4秒
-for i = 1: length(quick_freq)
-	quick_freq(i, 1) = sum(abs(AP_single_ANF((i- 1)* stride+ 1: (i- 1)* stride+ win_size))) / win_size;
+freq = zeros((length(AP_single_ANF)- win_size_freq)/ stride_freq+ 1, 1); % 1秒滑窗 数据量等于sample_rate/2 步长为0.4秒
+for i = 1: length(freq)
+	freq(i, 1) = sum(abs(AP_single_ANF((i- 1)* stride_freq+ 1: (i- 1)* stride_freq+ win_size_freq))) / win_size_freq;
 end
-freq_vote = max(quick_freq) > min(quick_freq)* 40; % 根据滑窗数据的最大最小均值进行频域判定
+freq_vote = max(freq) > min(freq)* freq_times; % 根据滑窗数据的最大最小均值进行频域判定
 
 end
--- matlab/quickdetection/presentdetection/time_detection.m
+++ matlab/quickdetection/presentdetection/time_detection.m
@@ -1,21 +1,23 @@
-function [time_vote] = quick_detection_time(data, win_size, stride)
+function [time_vote] = time_detection(data, win_size_time, stride_time, time_times, time_add)
 
 %{
-Function Name: quick_detection_time
+Function Name: time_detection
 Description: 根据窗内标准差返回时域判定结果
 Input: 
 	data: 一维数组
-	win_size: 窗长
-	stride: 步长
+	win_size_time: 时域窗长
+	stride_time: 时域步长
+	time_times: 时域乘法门限
+	time_add: 时域加法门限
 Output: None
 Return:
 	time_vote: 时域判定结果（布尔值）
 %}
 
-quick_time = zeros((length(data)- win_size) / stride+ 1, 1); % 计算窗数量
-for i = 1: length(quick_time)
-	quick_time(i, 1) = std(data((i- 1)* stride+ 1: (i- 1)* stride+ win_size)); % 窗内标准差
+time = zeros((length(data)- win_size_time) / stride_time+ 1, 1); % 计算窗数量
+for i = 1: length(time)
+	time(i, 1) = std(data((i- 1)* stride_time+ 1: (i- 1)* stride_time+ win_size_time)); % 窗内标准差
 end
-time_vote = max(quick_time) > min(min(quick_time)* 5, min(quick_time)+ 50); % 根据滑窗数据的最大最小标准差进行时域判定
+time_vote = max(time) > min(min(time)* time_times, min(time)+ time_add); % 根据滑窗数据的最大最小标准差进行时域判定
 
 end
