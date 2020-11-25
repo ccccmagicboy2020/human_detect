4163e327c921f5775ad333c74f43e3195dda46e3
cccc <cuiwei_cv@163.com>
2020-11-25 16:30:17 +0800

    use rfft and q15 function

--- stm32handC5_411CEU6/Src/freq_detection.c
+++ stm32handC5_411CEU6/Src/freq_detection.c
@@ -22,9 +22,10 @@ extern int gst_spectrum_fix_fft (fixed fr[], fixed fi[], int m, int inverse);
 extern void FFT(int dir,int points2, int32_t* real, int32_t* img);
 
 #define TEST_LENGTH_SAMPLES 4096
-float testInput[TEST_LENGTH_SAMPLES];
-float testInput2[TEST_LENGTH_SAMPLES];
+short testInput[TEST_LENGTH_SAMPLES];
+short testInput2[TEST_LENGTH_SAMPLES];
 float testInput3[TEST_LENGTH_SAMPLES];
+float testInput4[TEST_LENGTH_SAMPLES];
 
 /*
  * {
@@ -53,19 +54,27 @@ int freq_detection(const float data[], const float win[], int data_size, int win
 	int freq_vote;
 	int i;
 //	int j;
-	arm_rfft_fast_instance_f32 S;
+
+	arm_rfft_instance_q15 S;
 	
-	arm_rfft_fast_init_f32(&S, data_size);
+	arm_rfft_init_q15(&S, data_size, 0, 1);
 	
 	for(i=0; i<data_size; i++)
 	{
 			//testInput[i] = data[i] * win[i];
-			testInput[i] = data[i];
+			//j = i % 20;
+			testInput[i] = (short)(data[i] * 32768);
+			//testInput[i] = arm_sin_q15(1638*j);
 	}
 	
-  arm_rfft_fast_f32(&S, testInput, testInput2, 0); 
+	arm_rfft_q15(&S, testInput, testInput2);
 	
-	arm_cmplx_mag_f32(testInput2, testInput3, data_size);
+	for(i = 0; i < data_size; i++)
+	{
+			testInput4[i] = (float32_t)testInput2[i]/8;
+	}	
+	
+	arm_cmplx_mag_f32(testInput4, testInput3, data_size);
 
 	printf("fft abs value start:\r\n");
 	
