7fee895bd9198bc0c64087e15dcaebc2e5b53f33
cccc <cuiwei_cv@163.com>
2021-04-15 10:06:53 +0800

    add final burn script

--- HCf460/burn.bat
+++ HCf460/burn.bat
@@ -1,3 +1,10 @@
-JLink.exe burn.txt
-
-
+@echo off
+for /f "delims=" %%a in ('JLink.exe burn.txt') do set a=%%a
+if "%a%" == "Script processing completed."  (
+	@echo on
+    echo success
+) else (
+	@echo on
+    echo fail
+	pause
+)
