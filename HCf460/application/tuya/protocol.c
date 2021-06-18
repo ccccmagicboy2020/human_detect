268308ec53fc9738f39fe837197432e85a6a74a6
cccc <cuiwei_cv@163.com>
2021-06-18 18:09:44 +0800

    fix 4.5m and 5m range

--- HCf460/application/tuya/protocol.c
+++ HCf460/application/tuya/protocol.c
@@ -358,11 +358,11 @@ void load_ceiling_setup(int mode)
 		upssa0.ppp.quick_time_times = 2.12f;
 		upssa0.ppp.quick_time_add = 27.67f;
 		upssa0.ppp.quick_freq_times = 19.83f;
-		upssa0.ppp.slow_time_times = 2.12f;
+		upssa0.ppp.slow_time_times = 2.12f*1.2f;
 		upssa0.ppp.slow_time_add = 27.67f;
 		upssa0.ppp.slow_freq_times = 19.83f;
 		upssa0.ppp.res_times = 93.56f;
-		upssa0.ppp.offsetmin = 1.15f;
+		upssa0.ppp.offsetmin = 1.15f*1.05f;
 	}
 	else if (mode == 8)		//5.0M
 	{
@@ -371,7 +371,7 @@ void load_ceiling_setup(int mode)
 		upssa0.ppp.quick_freq_times = 17.54f;
 		upssa0.ppp.slow_time_times = 2.05f;
 		upssa0.ppp.slow_time_add = 21.50f;
-		upssa0.ppp.slow_freq_times = 4.0f;
+		upssa0.ppp.slow_freq_times = 17.54f;
 		upssa0.ppp.res_times = 85.85f;
 		upssa0.ppp.offsetmin = 1.08f;
 	}	
