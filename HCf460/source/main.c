d745cbc4dd7de866395987e4539a076045bfefb6
cccc <cuiwei_cv@163.com>
2021-07-04 22:41:47 +0800

    add min and max result when slow check prepare

--- HCf460/source/main.c
+++ HCf460/source/main.c
@@ -71,6 +71,8 @@ char JS_RTT_UpBuffer[1024];
 Val_t adc_value;
 ////////////////////////////////////////////////////////////
 float spp_result = 0;
+float mid_max = 0.00f;
+float mid_min = 0.00f;
 //float spp_result __attribute__((section(".ARM.__at_0x1FFF8D64"))) = 0;
 ////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////
@@ -528,6 +530,8 @@ void slow_check_data_prepare_s0(void)
 		}
 
 		arm_std_f32(data_temp, SLOW_CHECK_SAMPLES, &spp_result);
+		arm_max_f32(data_temp, SLOW_CHECK_SAMPLES, &mid_max, NULL);
+		arm_min_f32(data_temp, SLOW_CHECK_SAMPLES, &mid_min, NULL);
 		
 			switch (upssa0.ppp.load_radar_parameter)
 			{
@@ -567,7 +571,11 @@ void slow_check_data_prepare_s0(void)
 			}
 
 		sprintf(float_str, "spp_threshold: %.3lf - %.3lf\r\n", spp_result, spp_threshold);
-		SEGGER_RTT_printf(0, "%s", float_str);			
+		SEGGER_RTT_printf(0, "%s", float_str);		
+		sprintf(float_str, "mid max: %.3lf\r\n", mid_max);
+		SEGGER_RTT_printf(0, "%s", float_str);
+		sprintf(float_str, "mid min: %.3lf\r\n", mid_min);
+		SEGGER_RTT_printf(0, "%s", float_str);		
 
 		if (spp_result > spp_threshold)
 		{
