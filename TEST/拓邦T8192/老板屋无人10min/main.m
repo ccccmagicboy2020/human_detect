0159643e61e44f581b851a24562b202b5ec2e1cb
cccc <cuiwei_cv@163.com>
2021-07-15 14:27:24 +0800

    update raw adc data

--- TEST/拓邦T8192/老板屋无人10min/main.m
+++ TEST/拓邦T8192/老板屋无人10min/main.m
@@ -1,10 +1,10 @@
 %%%
-p0 = process('topband-p0.csv', 2, 1.02)
-p1 = process('topband-p1.csv', 2, 1.02)
-p2 = process('topband-p2.csv', 2, 1.02)
-p3 = process('topband-p3.csv', 2, 1.02)
-p4 = process('topband-p4.csv', 20, 1.02)
-p5 = process('topband-p5.csv', 20, 1.02)
+p0 = process('test-p0.csv', 2, 1.02)
+p1 = process('test-p1.csv', 2, 1.02)
+p2 = process('test-p2.csv', 2, 1.02)
+p3 = process('test-p3.csv', 2, 1.02)
+p4 = process('test-p4.csv', 20, 1.02)
+p5 = process('test-p5.csv', 20, 1.02)
 fid=fopen('result.txt','w');
 fprintf(fid,'p0: %.3f\r\n', p0);
 fprintf(fid,'p1: %.3f\r\n', p1);
